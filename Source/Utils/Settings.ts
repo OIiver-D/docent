0628911188cd59264cf6478df25f1b9afff26c6c
Oliver-D <olliedininno@gmail.com>
2024-08-24 15:52:06 -0400

    add webhook Setting

--- Source/Utils/Settings.ts
+++ Source/Utils/Settings.ts
@@ -6,15 +6,24 @@ class Settings {
 		Logger.Ok("Loading configuration settings.")
 		this.SettingsList = [
 			// Add settings items here
-			[ValidSetting.CoolDm, {
-				name: "Is the DM Cool?",
-				scope: "world", // or client
+			[ValidSetting.WebHookUrl, {
+				name: "Webhook URL",
+				scope: "world",
+				type: String,
+				hint: "This should be the Webhook's URL from the discord channel you want to send chat messages to. Leave it blank to have Foundry to Discord ignore regular chat messages.",
+				config: true,
+				default: "",
+				requiredReaload: true,
+			} as ClientSettings.PartialSetting],
+			[ValidSetting.AutoSyncJournal, {
+				name: "Auto Sync Journal Entries",
+				scope: "world",
 				type: Boolean,
-				hint: "Really think about it... is the DM cool?",
-				config: true, // It should appear in the configuration menu
-				default: false, // The DM is NOT cool by default
-				onChange: val => Logger.Ok("It has been deemed that the DM is " + (val ? "" : "NOT ") + "cool!"),
-			}]
+				hint: "Automatically sync journal entries to discord when they are created or updated.",
+				config: true,
+				default: true,
+				requiredReaload: true,
+			} as ClientSettings.PartialSetting]
 		];
 	}
 
@@ -48,7 +57,8 @@ class Settings {
 export const RegisterSettings = (): void => Settings.Get().RegisterSettings();
 
 export enum ValidSetting {
-	CoolDm = "coolDm"
+	WebHookUrl = "webHookURL",
+	AutoSyncJournal = "autoSyncJournal"
 }
 
 export const GetSetting = <T>(setting: ValidSetting): T | null => {
