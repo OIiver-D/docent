fa1d27dbaeafecee1df6fa6d9d9cb54b518763a3
Oliver-D <olliedininno@gmail.com>
2024-08-24 15:51:58 -0400

    add sync button

---
+++ Source/Hooks/hooks.ts
@@ -0,0 +1,13 @@
+import Logger from "../Utils/Logger";
+
+
+export async function initCustomHooks(): Promise<void> {
+	Hooks.on<Hooks.GetApplicationHeaderButtons<JournalSheet>>('getJournalSheetHeaderButtons', (sheet, buttons ) => {
+		buttons.unshift({
+			class: "sync-journal",
+			icon: "fas fa-rotate",
+			label: "Sync Journal",
+			onclick: () => {Logger.Log("Sync Journal Entry")},
+		});
+	})
+}
